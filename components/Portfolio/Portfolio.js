4fd7fb74b5780673ff43e54f78926fe14e9900a9
yalcinos <yalcintatar41@gmail.com>
2020-10-13 21:13:32 -0700

    Updates

--- components/Portfolio/Portfolio.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const Portfolio = () => {
-  return <div>COMING SOON...</div>;
-};
-export default Portfolio;
---
+++ precache-manifest.6ee3c3db47c36ac8657ebcd24d9beba4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "859e3a7c043175786341fd259bc3675d",
+    "url": "/yt-port/index.html"
+  },
+  {
+    "revision": "f12bdfd4f51625f72754",
+    "url": "/yt-port/static/css/2.22ca87da.chunk.css"
+  },
+  {
+    "revision": "9eb7fdeebfe1de0aa835",
+    "url": "/yt-port/static/css/main.8a4fb0ee.chunk.css"
+  },
+  {
+    "revision": "f12bdfd4f51625f72754",
+    "url": "/yt-port/static/js/2.ba10d1ea.chunk.js"
+  },
+  {
+    "revision": "4999ab4c2adcf2955c010315f478f47b",
+    "url": "/yt-port/static/js/2.ba10d1ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eb7fdeebfe1de0aa835",
+    "url": "/yt-port/static/js/main.f5de393c.chunk.js"
+  },
+  {
+    "revision": "406412f20fc92489c69f",
+    "url": "/yt-port/static/js/runtime-main.365cbc36.js"
+  }
+]);
