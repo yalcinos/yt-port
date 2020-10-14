74546d72d9d57c7c67f5f387cca335c04d9f84d7
yalcinos <yalcintatar41@gmail.com>
2020-10-14 15:58:31 -0700

    feat: Accordion added

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
 import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Avatar from "@material-ui/core/Avatar";
+import Chip from "@material-ui/core/Chip";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import Button from "@material-ui/core/Button";
 import Particles from "react-particles-js";
 import nodeJs from "../../assets/images/node-brands.svg";
 import reactLogo from "../../assets/images/icons8-react-native.svg";
@@ -21,71 +26,64 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 console.log(images1);
 const useStyles = makeStyles({
   root: {
-    maxWidth: 345,
-    // backgroundColor:"#05386B"
+    width: "80vw",
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: "20vh",
   },
   media: {
     height: 300,
+  },
+  accordionItem: {
+    backgroundColor: "#05386B",
+    color: "white",
+    textAlign: "center",
   },
 });
 
 const Portfolio = () => {
   const classes = useStyles();
-
   const [portfolioData, setPortfolioData] = useState();
   const [image, setImage] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  console.log(image[1]);
-  const a = image[1];
+
   useEffect(() => {
     setPortfolioData(data);
-    getImagesByProjectName("kovan");
+    setIsLoading(false);
   }, []);
 
-  const getImagesByProjectName = (projectName) => {
-    const imageArray = [];
-    if (projectName === "kovan") {
-      for (let i = 1; i <= 9; i++) {
-        import(`../../assets/images/${projectName}/${i}.png`).then((image) => {
-          imageArray.push(image);
-        });
-      }
-      setImage(imageArray);
-      setIsLoading(false);
-    }
-  };
-
   return (
-    <div>
-      {isLoading || !image.length ? (
+    <Grid container className={classes.root} spacing={2}>
+      {isLoading ? (
         <h1>LOADING...</h1>
       ) : (
-        <Card className={classes.root}>
-          <CardActionArea>
-            <CardMedia
-              className={classes.media}
-              image={image[1].default}
-              title="Contemplative Reptile"
-            />
-
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button size="small" color="primary">
-              View Source Code
-            </Button>
-          </CardActions>
-        </Card>
+        portfolioData.map((item, index) => {
+          return (
+            <Grid item xs={12} justify="center">
+              <Accordion>
+                <AccordionSummary
+                  className={classes.accordionItem}
+                  expandIcon={<ExpandMoreIcon style={{ color: "white" }} />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography className={classes.heading}>
+                    {item.projectName}
+                  </Typography>
+                  <Chip
+                    avatar={<Avatar src={reactLogo}></Avatar>}
+                    label="React"
+                  />
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>{item.projectDetails}</Typography>
+                </AccordionDetails>
+              </Accordion>
+            </Grid>
+          );
+        })
       )}
-    </div>
+    </Grid>
   );
 };
 export default Portfolio;
