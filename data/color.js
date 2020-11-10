e0480afda8e71695ff511ac34a4290e49434be2a
yalcinos <yalcintatar41@gmail.com>
2020-11-09 18:54:14 -0800

    refactor: Update gitignore

---
+++ data/color.js
@@ -0,0 +1,26 @@
+export const themeColor = [
+  "#e2627d",
+  "#ffd7ae",
+  "#9ad9ff",
+  "#fec5e3",
+  "#9fc1ff",
+  "#74ffd6",
+  "#ff97a2",
+  "#a5d0e6",
+  "#03c4a1",
+  "#79d1e0",
+];
+export const aboutMeColor = [
+  "#05386B",
+  "#ea9438",
+  "#d7385e",
+  "#000000",
+  "#de2b2b",
+  "#335d2d",
+  "#590995",
+  "#03c4a1",
+  "#726a95",
+  "#1e5f74",
+  "#ea9438",
+  "#79d1e0",
+];
